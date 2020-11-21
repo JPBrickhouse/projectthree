2d28bcdf2cb0769aecb49c7d348bc56b7af4f63e
JBrickhouse <jbrickhouse777@gmail.com>
2020-11-20 19:16:10 -0600

    React Leaflet map basic implementation

---
+++ client/src/components/CovidMap/index.js
@@ -0,0 +1,54 @@
+// Importing React
+import React from 'react';
+
+// Importing properties from "react-leaflet" by using destructuring
+import { MapContainer, GeoJSON, TileLayer } from "react-leaflet"
+
+// Importing the necessary CSS to display the React Leaflet map
+import "leaflet/dist/leaflet.css"
+
+// This is the CovidMap function
+const CovidMap = ({ stateGeom }) => {
+    // - It assigns that name and "binds" it to the map layer
+    // - This is done in tandom with the onEachFeature property of <GeoJSON/> below
+    const onEachUSstate = (feature, layer) => {
+        // This function goes into the feature (aka, the stateGeom data object)
+        // and looks for the .properties.name, and then assigns it to usstatename
+        const usstatename = feature.properties.name
+        // layer.bindPopup binds that usstatename to a popup that appears when clicked
+        layer.bindPopup(`${usstatename}`)
+        // layer.on click runs the function "clickToFeature" when a usstate is clicked
+        layer.on({
+            click: clickToFeature
+        })
+    }
+
+    // https://stackoverflow.com/questions/41950931/registering-event-with-react-leaflet
+    // The clickToFeature function runs when a usstate is clicked
+    const clickToFeature = (event) => {
+        // The event target (aka, what was targeted by the click event)
+        // is the particular US State, so grab the feature.properties.name associated
+        
+        // FOR NOW...
+        console.log(event.target);
+        console.log("I clicked on " + event.target.feature.properties.name);
+
+        // EVENTUALLY...
+        // Pass this information back up to the parent
+
+    }
+
+    // This is the return of the function, which displays the map
+    return (
+        <MapContainer style={{ height: "50vh", width: "50%" }} zoom={4} center={[39, -96]}>
+            <GeoJSON data={stateGeom} onEachFeature={onEachUSstate} />
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
+    );
+}
+
+// Exporting the function
+export default CovidMap;
